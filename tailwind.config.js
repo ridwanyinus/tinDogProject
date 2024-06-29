f1af911945d48bf13bf79cb6eaca64ec26d37f4c
Ridwan <ridwanyinusa15@gmail.com>
2024-06-29 10:51:32 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}", "./index.html", "./src/**/*.{html,js}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
